4979bfc2ebc47ceeedb45e990199acd34177d198
sid978123 <siddharthaman978@gmail.com>
2026-03-31 16:22:00 +0530

    loops and some Basic questions is done :

---
+++ 26.py
@@ -0,0 +1,128 @@
+#Arithimatic Operators.. -> These operations are used to perform mathematical operations on numbers.
+
+p = 2 
+q = 4
+result = p + q
+print(result)
+
+#Floor division Operator..
+ 
+a = 5
+result = a//2
+print(result)
+
+#Exponet Operator..
+
+result = p**q
+print(result)
+
+
+#modulo Operator
+
+result = q % p
+print(result)
+
+#ASSIGNEMENT oPERATOR 
+  #1  Simple Assignment Operator 
+# x = 2
+# x = x + 1
+# x = 4/2
+# print(x)
+
+# p = 4
+# # p *= 5
+# p -= 4
+# p /= 5
+# p //= 5
+# p %= 4
+
+# print( p)
+
+
+
+
+
+
+
+#Logical Operator -> AND , OR  ,NOT
+
+#precedence and Associativity
+
+# result = ( 2 + 3 - 5 * 9 // 2  )
+# print(result)
+# string = '''Mikasa like girl 
+# can not found in real world and knowing this fact yet my heart whisper for her and It is not in my controll'''
+
+
+#Indentation
+
+# name = "siddharth" 
+#   result(name)
+#   print(result)
+
+# def greet(name):
+#   print(name)
+
+# greet("siddharth")
+
+#Taking Input from th User
+
+# age = input("Enter age of the user :" " ")
+# print( "The User age is : " " " , age)
+
+# name = "sid" 
+# age = 32
+# print("My name is {0} and age is {1}".format("sid" , 21))
+# print("My name is {name} and age is {age}".format(name = "sid" ,age = 21))
+
+
+# country = input("Which country do you live in : " " ")
+# print("I live in {0}".format(country))
+
+
+
+
+# print("My name is {name}".format(name = "Akshu"))
+
+
+name = "Akshu"
+age = 23
+print(f"Her name is {name}")
+
+# r = int(input("Enter the radius of the circle : " " "))
+# area = 3.14*pow( r , 2)
+# c  = 2*3.14*r
+# print(f"The circumference of the circle is : {c} " " ")
+# print(f"The area of the circle is : {area}")
+
+
+Question = ''' Write Python Program to Convert the Given Number of Days 
+to a Measure of Time Given in Years, Weeks and Days. For Example, 
+375 Days Is Equal to 1 Year, 1 Week and 3 Days (Ignore Leap Year).'''
+
+#days = int(input("Enter the Number of days :"))
+# years = ( days / 365)
+# weeks =((days % 365) / 7)
+# remaining_days =((days % 365) % 7)
+
+# years = days // 365
+# days = days % 365
+# weeks = days // 7
+# days = days % 7
+# print(f"The years are :{years}  , weeks are : {weeks} and Remaining days are : {days}")
+
+#Type conversion : 
+''' float , string to INT and 
+    int , string to FLOAT'''
+
+# name = "sid"
+# result = str(name)
+# print(result)
+# print(type(result))
+
+
+
+ascii_to_char = chr(78)
+print(f'Equivalent Character for ASCII value of {ascii_to_char} is {ascii_to_char}')
+
+comp
---
+++ main.py
@@ -0,0 +1,81 @@
+# print("Hello, World!")
+# print("Hello:" ,1,2 , sep='/')
+# print("hello" , end='-')
+# print("man")
+
+# print(True , False,None)
+
+# print([1 , 2, 34,4]) #list
+# print((1 , 2, 3,4, 5, 5)) #tuple
+# print({1, 2, 3, 4, 5}) #sets
+# print({"name":"sid "  , "age " : 23}) #dict
+
+
+#type function
+# print(type(1))
+
+
+#variables 
+
+# name = "sid"
+# print(name)
+
+# age = 12
+# print(age)
+
+# a=b=c=4 
+# print( a , b, c)
+
+#python support dynamic typing because we don't need to declare the type of variable before using it. we can change the type of variable at any time.
+
+#other languages support static typing because we need to declare the type of variable before using it. we can't change the type of variable at any time.
+
+
+
+#keywords in python and identifiers
+
+first_name = "sid" 
+
+
+
+fnum = input('Enter your number :')
+snum = input('Enter your first number')
+
+result = int(fnum) + int(snum)
+print(result)
+
+
+#Type conversion
+ #implicit and explicit type conversion
+    # implicit type conversion is done by the interpreter automatically when we perform an operation on two different types of data. for example, when we add an integer and a float, the interpreter will convert the integer to a float before performing the addition.
+
+    # explicit type conversion is done by the programmer using built-in functions like int(), float(), str(), etc. for example, if we want to convert a string to an integer, we can use the int() function.
+
+# name = "sid"
+name1 = input("Enter your name ")
+name2 = input("Enter Your 2nd name ")
+result = name1 + " " + name2
+print(result  )
+
+type(result)
+
+
+#Litterals    --->>   in python are the values that we use in our code. they can be of different types like string, integer, float, boolean, etc. for example, "hello" is a string literal, 123 is an integer literal, 3.14 is a float literal, and True is a boolean literal. literals are used to represent data in our code and can be assigned to variables or used directly in expressions.
+
+
+a = 0b1010 #binary literal
+b = 0o12 #octal literal
+c = 0x1A #hexadecimal literal
+
+d = float_2 = 1.5e2
+print( a , b , c ,d)
+
+#Booleans
+
+
+k = None
+a = 32
+b = 32
+print(a , b)
+
+
