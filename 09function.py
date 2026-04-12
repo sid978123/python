49b423ea98ca847f14c8264290580cc2a4dedf23
sid978123 <siddharthaman978@gmail.com>
2026-04-12 18:29:06 +0530

    did some topics like map , function , reduce , filter :

--- 09function.py
+++ 09function.py
@@ -1,1 +1,166 @@
-''''''
+'''Decomposition :'''
+def is_even(num):
+
+  '''This   function returns if a given number is odd or even 
+  input - any valid integer
+  output - odd/even
+  created on - 09th april 2026'''
+  if num % 2 == 0 :
+    return 'even'
+  else :
+    return 'odd'
+
+
+
+for i in range(1 , 11) :
+  x = is_even(i)
+  print(x) 
+'''Arguments 
+1. Default 
+2. Positional argument
+3. keyword argument :'''
+
+
+def power(a=1 , b=1):
+  return a**b
+
+s = power(8 , 2)
+print(s)
+s = power(a=8 ,b=2) #Positional argument ; 
+print(s)
+s = power(b=8 , a=2) #keyword argument : 
+print(s)
+
+
+'''Args and kwargs'''
+
+'''*args -> it allows us to pass a variable number of nonkeyword arguments to a function .'''
+
+def multiply( a , b  , c) :
+  return a*b*c
+
+
+s = multiply(1 , 2, 3)
+print(s)
+
+def maltily(*args):
+  product = 1 
+
+  for i in args:
+    product *= i
+
+  return product
+
+m = maltily(1 ,2 , 3, 4,7 ,5 , 6 ,1,2,3,3,4)
+print(m)
+print(is_even.__doc__)
+
+'''kwargs'''
+def display(**kwargs):
+  for (key , value) in kwargs.items():
+    print(key , value)
+
+
+s=display(india="delhi",pak="islamabad",pakk='Lahore')
+
+
+
+'''Global variable and Local variable : '''
+
+
+'''Nested Functions : '''
+def f():
+  def g():
+    print('inside function g')
+    f()
+
+  g()
+  print('inside function f')
+
+
+'''Functions as first class citizens : '''
+
+def square(num) :
+  return num**2
+
+
+x = square(8)
+print(x)
+
+'''Returning a function : '''
+
+def f() :
+  def x ( a , b) :
+
+    return a + b 
+  return x 
+val = f() (3 ,4)
+print(val)
+
+
+'''Benifits of a function  : 
+1. code moduability 
+2. code Reusability
+3. code Readability'''
+
+
+'''lambda Function :
+a lambda function is a small
+it is written in one line  '''
+
+a = lambda x , y : x + y
+print( a(2 , 3))
+
+s = lambda s: 'a' in s
+b = s('helloo')
+print(b)
+
+
+
+a = lambda x : 'even' if x%2 == 0 else 'odd'
+s = a(6)
+print(s)
+
+
+'''Higher order function :'''
+
+
+def transform(f , L ) :
+  output = []
+  for i in L :
+    output.append(f(i))
+
+  print(output)
+
+
+L = [ 1 , 2 ,3 , 4, 5]
+transform(lambda x :x**3 , L)
+
+
+
+
+'''Map function  :'''
+a = list(map(lambda x : x**2 , [ 1 ,2 ,3 , 4,5]))
+print(a)
+
+
+'''filter '''
+L =  [ 1 ,2 , 3, 4 , 5]
+a = list(filter(lambda x:x>3 , L))
+print(a)
+
+
+
+
+'''Reduce :'''
+
+import functools
+a = functools.reduce(lambda x , y : x+y , [ 1 , 2 ,3 , 4, 5])
+
+print(a)
+
+a = functools.reduce(lambda x , y :x if x>y else y , [ 11 , 22, 44 , 33 , 55 ])
+print(a)
+
+a =min([ 11 , 22, 44 , 33 , 55 ])
+print(a)
--- task04.py
+++ task04.py
@@ -1,66 +1,73 @@
-'''combine two list index wise'''
+# '''combine two list index wise'''
 
-list1 = ['M', 'na', 'i', 'Kh']
-list2 = ['y', 'me', 's', 'an']
+# list1 = ['M', 'na', 'i', 'Kh']
+# list2 = ['y', 'me', 's', 'an']
 
-L = []
+# L = []
 
-for i in range(len(list1)):
-    L.append([list1[i], list2[i]])
+# for i in range(len(list1)):
+#     L.append([list1[i], list2[i]])
 
-print(L)
+# print(L)
 
-'''add new item to list after a specific item '''
+# '''add new item to list after a specific item '''
 
-L1 = [10 , 20 , [300 , 400 , [ 5000 , 6000] , 500] , 30 , 40]
-L1[2][2].append(7000)
-print(L1)
-
-
-'''write a program to show no of items of each candy type'''
-
-candy_list = ['Jelly Belly', 'Kit Kat', 'Double Bubble', 'Milky Way', 'Three Musketeers']
-no_of_items = [10, 20, 34, 74, 32]
-L = []
-for i in range(len(candy_list)):
-    L.append([candy_list[i] , no_of_items[i]])
-
-print(L)
+# L1 = [10 , 20 , [300 , 400 , [ 5000 , 6000] , 500] , 30 , 40]
+# L1[2][2].append(7000)
+# print(L1)
 
 
-'''Running sum on list Write a programm  to print a list after performing running sum on it'''
+# '''write a program to show no of items of each candy type'''
 
-list1 = [ 1 ,2 , 3, 4, 5, 6]
-sum = 0
-L = []
-for i in list1:
-    sum += i
-    L.append(sum)
-print(L)
+# candy_list = ['Jelly Belly', 'Kit Kat', 'Double Bubble', 'Milky Way', 'Three Musketeers']
+# no_of_items = [10, 20, 34, 74, 32]
+# L = []
+# for i in range(len(candy_list)):
+#     L.append([candy_list[i] , no_of_items[i]])
 
-
-''''''
-list1 = [ 2 , 4 , 6, 10 , 1]
+# print(L)
 
 
-L = []
-for i in list1:
-    sum = 0
-    for j in list1:
-        if i<=j:
-            sum += j
-    L.append(sum)
+# '''Running sum on list Write a programm  to print a list after performing running sum on it'''
+
+# list1 = [ 1 ,2 , 3, 4, 5, 6]
+# sum = 0
+# L = []
+# for i in list1:
+#     sum += i
+#     L.append(sum)
+# print(L)
+
+
+# ''''''
+# list1 = [ 2 , 4 , 6, 10 , 1]
+
+
+# L = []
+# for i in list1:
+#     sum = 0
+#     for j in list1:
+#         if i<=j:
+#             sum += j
+#     L.append(sum)
       
-print(L)        
+# print(L)        
 
 
-'''find list of common unique items from two list and show in increasing order : '''
-    
+list1 = [1, 7, 3, 4, 5, 10]
+list2 = [4, 5, 6, 7, 8, 9]
 
+L = []
+for i in list1:
+    for j in list2:
+        if i == j:
+            # Check if it's already in L to ensure uniqueness
+            if i not in L:
+                L.append(i)
 
-
-
-
+L.sort() # Sort once at the end
+print(L) 
+# Output: [4, 5, 7]
 
 
 
