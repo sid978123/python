2fdf5d202fb64ee8c84d82a57ff4527a82c6a200
sid978123 <siddharthaman978@gmail.com>
2026-06-18 23:06:53 +0530

    did soem ques

---
+++ revise_python.py
@@ -0,0 +1,25 @@
+score = lambda x : x**2
+print(score(5))
+
+add = lambda x , y : x + y
+print(add(2 , 3))
+
+max = lambda x , y : x > y
+print(max(2 , 4))
+
+
+numbers = [ 1,2,3,4,5,6]
+square = []
+square = list(map(lambda x : x*x , numbers))
+print(square)
+
+
+
+'''Here we a re going to solve a prob that is related to the temperature . like we are goingt o convert foreigheight into celcious '''
+
+
+int
+forr = [ 13 , 443, 45,65]
+cel = list(map(lambda x:x*(9/5)  + 32 , forr))
+print(cel)
+
